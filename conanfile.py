a2b4bf2becbf6fdeac8580a327365d70197cbfe1
Michael Tao <michael.tao@nyu.edu>
2025-07-26 11:19:19 -0400

    initial attempt at adding zipper for a downstream project, will unit test there first

---
+++ conanfile.py
@@ -0,0 +1,29 @@
+from conan import ConanFile
+from conan.tools.meson import MesonToolchain
+from conan.tools.gnu import PkgConfigDeps
+import os
+
+__BASE_DEPS__ = [
+        "spdlog/1.15.1", 
+        "range-v3/0.12.0",  
+        "catch2/3.8.1",
+        "mdspan/0.6.0"
+        ]
+
+
+
+
+
+class mshio(ConanFile):
+    requires = __BASE_DEPS__ 
+    settings = "os", "compiler", "build_type"  
+    generators = "PkgConfigDeps"
+
+
+
+                                               
+    def generate(self):
+        meson = MesonToolchain(self)                   
+        meson.generate()
+
+                                               
